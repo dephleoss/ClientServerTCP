6c4c9c3b9fd6fba6d1764f8b129985ea358a9184
dephleoss <dipi.leonardo@gmail.com>
2020-01-20 09:09:15 +0100

    clientserver

---
+++ clientserver/src/clientserver/ProgClient.java
@@ -0,0 +1,26 @@
+package clientserver;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ProgClient {
+    public static void main(String Argv[]){
+        Socket socket=null;
+        String indirizzo=null;
+        try {
+            socket = new Socket("localhost",2000);
+            indirizzo = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(indirizzo);
+            System.out.println(socket.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
